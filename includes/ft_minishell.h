a368505eebf6b5774b30842f7159ff8b9f4b6a37
FunkyOctopus <>
2021-10-06 12:41:50 +0200

    changed everything

--- includes/ft_minishell.h
+++ includes/ft_minishell.h
@@ -1,4 +1,18 @@
 #ifndef FT_MINISHELL_H
 # define FT_MINISHELL_H
 
+# include <curses.h>
+# include <dirent.h>
+# include <fcntl.h>
+# include <readline/readline.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <signal.h>
+# include <stdio.h>
+# include <sys/stat.h>
+# include <sys/ioctl.h>
+# include <sys/wait.h>
+# include <termios.h>
+# include <unistd.h>
+
 #endif
