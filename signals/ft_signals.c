1b8e18527d8b855c193221a42809599c56581af5
Jonathan Fritz <jfritz@1-B-5.42heilbronn.de>
2021-10-07 11:15:39 +0200

    Doing weird jfritz stuff

--- signals/ft_signals.c
+++ signals/ft_signals.c
@@ -3,23 +3,15 @@
 /*
 **	Catches all signals and decides what to do
 */
-static void	ft_handle(int sig, siginfo_t *siginfo, void *context)
+static void	ft_handle(int sig)
 {
-	if (sig == SIGUSR2)
-		ft_sig_convert(1);
-	else if (sig == SIGUSR1)
-		ft_sig_convert(0);
-	if (context)
-		sig = 1;
-	kill(siginfo->si_pid, SIGUSR1);
+	if (sig == SIGINT)
+		printf("\nHELLO");
 }
 
 int	ft_setup_signals()
 {
-	struct sigaction	sig_catcher;
-
-	sigemptyset(&sig_catcher.sa_mask);
-	sig_catcher.sa_flags = 0x0040;
-	sig_catcher.sa_sigaction = ft_handle;
-	sigaction (SIGUSR1, &sig_catcher, NULL);
+	signal (SIGINT, ft_handle);
+	pause();
+	return (0);
 }
