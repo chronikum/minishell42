80d7bdb6ea8923c01069df3b7f5e7f88b8078569
Jonathan Fritz <jonathanfritz@AirvonJonathan.fritz.box>
2021-11-07 19:37:55 +0100

    Prompt now has unicode symbols in it. Looks very cute

--- prompt/ft_nice_prompt.c
+++ prompt/ft_nice_prompt.c
@@ -10,12 +10,14 @@ char *ft_get_nice_prompt(void)
 	char	*command_prompt;
 	char	*colored_part;
 	char	*username;
+	char	*user_at_char;
 
 	username = "";
+	user_at_char = ft_gc_strjoin(ft_gc_strjoin(YELLOW_COLOR, "⇎"), RESET_COLOR);
 	if (ft_find_envlist("USER"))
 		username = ft_find_envlist("USER")->value;
 	user = ft_gc_strjoin(ft_gc_strjoin(MAGENTA_COLOR, username), RESET_COLOR);
-	colored_part = "minishell\033[0m \033[0;36m$\033[0m> ";
-	command_prompt = ft_gc_strjoin(ft_gc_strjoin(user, "@"), colored_part);
+	colored_part = "minishell\033[0m \033[0;36m⇉\033[0m ";
+	command_prompt = ft_gc_strjoin(ft_gc_strjoin(user, user_at_char), colored_part);
 	return (command_prompt);
 }
