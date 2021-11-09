151b0092ed13d3a1d58bb24c9653d484f1e3ac77
FunkyOctopus <>
2021-11-09 11:30:10 +0100

    cd error messages

--- builtins/ft_cd.c
+++ builtins/ft_cd.c
@@ -3,7 +3,13 @@
 int	ft_cd(t_command *command)
 {
 	ft_setenv("PWD", command->args[1]);
+	//access
 	if (chdir(command->args[1]) == -1)
-		printf("bash: cd: %s: Not a directory\n", command->args[1]);
+	{
+		if (access(command->args[1], F_OK) == -1)
+			printf("bash: cd: %s: No such file or directory\n", command->args[1]);
+		else
+			printf("bash: cd: %s: Not a directory\n", command->args[1]);
+	}
 	return(0);
 }
