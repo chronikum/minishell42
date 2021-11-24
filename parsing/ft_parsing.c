91cc5593fb00a47b67bd0f527f66a6d26a3f10d8
Jonathan Fritz <fritz@nosc.io>
2021-11-24 16:10:02 +0100

    Debug is now less confusing

--- parsing/ft_parsing.c
+++ parsing/ft_parsing.c
@@ -1,25 +1,33 @@
 #include "../includes/ft_minishell.h"
 
 
-void	ft_command_debug(t_command *command_struct)
+void	ft_command_debug(t_command *start)
 {
-	if (DEBUG)
+	t_command *command_struct;
+	
+	command_struct = start;
+	
+	while (command_struct)
 	{
-		printf("%sSECTION     %s\n", BACKGROUND_BLUE, RESET_COLOR);
-		printf("	Command: 			%s\n", command_struct->command);
-		printf("	original_string: 		%s\n", ft_gc_strtrim(command_struct->original_string, "<>| "));
-		ft_arg_printer(command_struct->args);
-		if (command_struct->file)
+		if (DEBUG)
 		{
-			printf("	file: 				%s|\n", command_struct->file);
-			printf("	FILE PATH LENGTH : 		%lu\n", ft_strlen(command_struct->file)); /// as we know the file name changes in length occassionally
+			printf("%sSECTION     %s\n", BACKGROUND_BLUE, RESET_COLOR);
+			printf("	Command: 			%s\n", command_struct->command);
+			printf("	original_string: 		%s\n", ft_gc_strtrim(command_struct->original_string, "<>| "));
+			ft_arg_printer(command_struct->args);
+			if (command_struct->file)
+			{
+				printf("	file: 				%s|\n", command_struct->file);
+				printf("	FILE PATH LENGTH : 		%lu\n", ft_strlen(command_struct->file)); /// as we know the file name changes in length occassionally
+			}
+			printf("	files: 			\n");
+			ft_print_files(command_struct->files);
+			printf("	in_flag: 			%d	%s\n", command_struct->in_flag, ft_translate_flags(command_struct->in_flag));
+			printf("	out_flag: 			%d	%s\n", command_struct->out_flag, ft_translate_flags(command_struct->out_flag));
+			printf("	builtin_sys_flag:		%d	%s\n", command_struct->builtin_sys_flag, ft_translate_flags(command_struct->builtin_sys_flag));
+			printf("%sSECTION END %s\n", BACKGROUND_BLUE, RESET_COLOR);
 		}
-		printf("	files: 			\n");
-		ft_print_files(command_struct->files);
-		printf("	in_flag: 			%d	%s\n", command_struct->in_flag, ft_translate_flags(command_struct->in_flag));
-		printf("	out_flag: 			%d	%s\n", command_struct->out_flag, ft_translate_flags(command_struct->out_flag));
-		printf("	builtin_sys_flag:		%d	%s\n", command_struct->builtin_sys_flag, ft_translate_flags(command_struct->builtin_sys_flag));
-		printf("%sSECTION END %s\n", BACKGROUND_BLUE, RESET_COLOR);
+		command_struct = command_struct->next;
 	}
 }
 
@@ -36,7 +44,6 @@ t_command	*ft_create_new_command(char *command_section)
 	ft_set_builtin_flag(command);
 	ft_set_flags(command);
 	command->files = ft_create_file_list(command_section);
-	ft_command_debug(command);
 	return (command);
 }
 
@@ -79,5 +86,7 @@ t_command	*ft_parse_in_commands(char *cmds)
 	);
 	if (ft_get_last_command(list)->out_flag == 0)
 		ft_get_last_command(list)->out_flag = -1;
+		
+	ft_command_debug(list);
 	return (list);
 }
