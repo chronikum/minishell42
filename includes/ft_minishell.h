e8b1839a509bd24b3190af54ecc020c1a9dde592
Jonathan Fritz <fritz@nosc.io>
2021-11-25 14:20:33 +0100

    Adding single quote detection helper function

--- includes/ft_minishell.h
+++ includes/ft_minishell.h
@@ -194,7 +194,9 @@ void		set_shell_envvariable(void);
 t_files		*ft_create_file_list(char *command);
 void		ft_print_files(t_files *files);
 char		*ft_translate_envs(char *command);
-
 char		*ft_insert_exit_code(char *command);
+
+void	ft_increase_i_singlequote_handler(char *cmd, int *i, int *quote);
+void	ft_increase_ui_singlequote_handler(char *cmd, unsigned int *i, int *quote);
 
 #endif
