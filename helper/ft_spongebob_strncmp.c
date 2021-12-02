eca89bc1c3af8188f6bfca8c4714f4cc232acc1c
Jonathan Fritz <fritz@nosc.io>
2021-12-02 13:57:45 +0100

    int	ft_strcmp_len(char *s1, char *s2)

--- helper/ft_spongebob_strncmp.c
+++ helper/ft_spongebob_strncmp.c
@@ -19,3 +19,13 @@ int	ft_sb_strcmp(char *s1, char *s2)
 		return (-1);
 	return (ft_strcmp(ft_strtolowercase(s1), ft_strtolowercase(s2)));
 }
+
+/*
+	Compars string and also checks for same length.
+*/
+int	ft_strcmp_len(char *s1, char *s2)
+{
+	if (ft_strlen(s1) != ft_strlen(s2))
+		return (-1);
+	return (ft_strcmp(s1, s2));
+}
