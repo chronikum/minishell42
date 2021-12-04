d3722be9ef67549ae2a03e5e1148187ec7b50d2b
FunkyOctopus <>
2021-12-04 18:06:55 +0100

    norm

--- parsing/ft_splint_aux.c
+++ parsing/ft_splint_aux.c
@@ -1,6 +1,7 @@
 #include "../includes/ft_minishell.h"
 
-void	ft_increase_until_change(int *q1, int *q2, int *q3, char *temp, unsigned int *i)
+void	ft_increase_until_change(
+		int *q1, int *q2, int *q3, char *temp, unsigned int *i)
 {
 	while (!(*q1) || !(*q2))
 	{
