ed81bce95e274509c9ad967278a17a2dbd9253fa
FunkyOctopus <>
2021-12-04 18:04:30 +0100

    norm

--- parsing/ft_parsing.c
+++ parsing/ft_parsing.c
@@ -11,19 +11,28 @@ void	ft_command_debug(t_command *start)
 		{
 			printf("%sSECTION     %s\n", BACKGROUND_BLUE, RESET_COLOR);
 			printf("	Command: 			%s\n", command_struct->command);
-			printf("	original_string: 		%s\n", ft_gc_strtrim(command_struct->original_string, "<>| "));
-			printf("	delimiter: 				%s\n", command_struct->delimiter);
+			printf("	original_string: 		%s\n",
+				ft_gc_strtrim(command_struct->original_string, "<>| "));
+			printf("	delimiter: 				%s\n",
+				command_struct->delimiter);
 			ft_arg_printer(command_struct->args);
 			if (command_struct->file)
 			{
 				printf("	file: 				%s|\n", command_struct->file);
-				printf("	FILE PATH LENGTH : 		%lu\n", ft_strlen(command_struct->file));
+				printf("	FILE PATH LENGTH : 		%lu\n",
+					ft_strlen(command_struct->file));
 			}
 			printf("	files: 			\n");
 			ft_print_files(command_struct->files);
-			printf("	in_flag: 			%d	%s\n", command_struct->in_flag, ft_translate_flags(command_struct->in_flag));
-			printf("	out_flag: 			%d	%s\n", command_struct->out_flag, ft_translate_flags(command_struct->out_flag));
-			printf("	builtin_sys_flag:		%d	%s\n", command_struct->builtin_sys_flag, ft_translate_flags(command_struct->builtin_sys_flag));
+			printf("	in_flag: 			%d	%s\n",
+				command_struct->in_flag,
+				ft_translate_flags(command_struct->in_flag));
+			printf("	out_flag: 			%d	%s\n",
+				command_struct->out_flag,
+				ft_translate_flags(command_struct->out_flag));
+			printf("	builtin_sys_flag:		%d	%s\n",
+				command_struct->builtin_sys_flag,
+				ft_translate_flags(command_struct->builtin_sys_flag));
 			printf("%sSECTION END %s\n", BACKGROUND_BLUE, RESET_COLOR);
 		}
 		command_struct = command_struct->next;
