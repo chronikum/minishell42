c9539199af75182a648dd0f1b601cc2c28cc077f
Jonathan Fritz <fritz@nosc.io>
2021-12-05 14:46:16 +0100

    Changes to file list

--- filelist/ft_create_file_list.c
+++ filelist/ft_create_file_list.c
@@ -6,7 +6,7 @@
 /*   By: jfritz <jfritz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/04 17:28:37 by olgerret          #+#    #+#             */
-/*   Updated: 2021/12/05 14:36:06 by jfritz           ###   ########.fr       */
+/*   Updated: 2021/12/05 14:45:18 by jfritz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,7 @@ t_files	*ft_new_file(char *filename, char *path, int in, int out)
 /*
 	Creates a file list depening on the command char array input
 */
-t_files	*ft_create_file_list(char *command)
+t_files	*ft_create_file_list(char *com)
 {
 	char		**result;
 	int			i;
@@ -58,9 +58,8 @@ t_files	*ft_create_file_list(char *command)
 	i = 1;
 	start = NULL;
 	files = NULL;
-	ft_file_str = ft_substr(command,
-			ft_strlen_not_any_quoted(command, "<>"), ft_strlen(command)
-			- ft_strlen_not_any_quoted(command, "<>"));
+	ft_file_str = ft_substr(com, ft_strlen_not_any_quoted(com, "<>"),
+			ft_strlen(com) - ft_strlen_not_any_quoted(com, "<>"));
 	result = ft_file_splitter(ft_file_str, "<>");
 	if (!result)
 		return (NULL);
@@ -68,15 +67,13 @@ t_files	*ft_create_file_list(char *command)
 	{
 		if (files)
 		{
-			files->is_multiple = !!(ft_detect_mredirections(command) - 1);
 			files->next = ft_new_file(result[i], result[i], 0, 0);
-			files->is_last = 0;
 			files = files->next;
 		}
 		else
 		{
 			files = ft_new_file(result[i], result[i], 0, 0);
-			files->is_multiple = !!(ft_detect_mredirections(command) - 1);
+			files->is_multiple = !!(ft_detect_mredirections(com) - 1);
 			files->is_last = 0;
 			start = files;
 		}
