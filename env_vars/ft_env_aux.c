a57921075c173ba0c8d7e35bb25a3e5bfd79b526
Jonathan Fritz <fritz@nosc.io>
2021-12-05 15:12:31 +0100

    Fix before fixing ft file splitter

--- env_vars/ft_env_aux.c
+++ env_vars/ft_env_aux.c
@@ -5,9 +5,9 @@
 	char and increasing both total char count and
 	iterator through the existing string
 */
-void ft_strncincrtotaluihand(char *command, t_envit *h)
+void	ft_strncincrtotaluihand(char *command, t_envit *h)
 {
 	ft_strncat(h->result, &command[h->i], 1);
 	h->total++;
-	ft_incs_uihand(command, &h->i, &h->qclosed);	
+	ft_incs_uihand(command, &h->i, &h->qclosed);
 }
